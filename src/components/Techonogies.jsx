d438cd6a7fa43a5aaca4cdfacb44fee2e2eb56f3
Devendra43 <devendran.pmu@gmail.com>
2024-10-05 17:47:56 +0530

    Added enhancements

--- src/components/Techonogies.jsx
+++ src/components/Techonogies.jsx
@@ -31,7 +31,8 @@ const Techonogies = () => {
                 whileInView={{ opacity: 1, y: 0 }}
                 initial={{ opacity: 0, y: -100 }}
                 transition={{ duration: 1.5 }}
-                className="my-20 text-center text-7xl">Technologies</motion.h1>
+                className="my-20 text-center text-7xl">Technologies
+            </motion.h1>
             <motion.div
                 whileInView={{ opacity: 1, x: 0 }}
                 initial={{ opacity: 0, x: -100 }}
@@ -43,6 +44,7 @@ const Techonogies = () => {
                     animate="animate"
                     className="rounded-2xl border-4 border-neutral-800 p-4">
                     <TbBrandJavascript className="text-7xl text-yellow-400" />
+                    <p className="text-center mt-2 content-evenly">Javascript</p>
                 </motion.div>
                 <motion.div
                     variants={iconVariants(3)}
@@ -50,6 +52,7 @@ const Techonogies = () => {
                     animate="animate"
                     className="rounded-2xl border-4 border-neutral-800 p-4">
                     <RiNodejsLine className="text-7xl text-green-600" />
+                    <p className="text-center mt-2 content-evenly">Node.JS</p>
                 </motion.div>
                 <motion.div
                     variants={iconVariants(5)}
@@ -57,6 +60,7 @@ const Techonogies = () => {
                     animate="animate"
                     className="rounded-2xl border-4 border-neutral-800 p-4">
                     <SiExpress className="text-7xl text-neutral-100" />
+                    <p className="text-center mt-2 content-evenly">Express.JS</p>
                 </motion.div>
                 <motion.div
                     variants={iconVariants(2.5)}
@@ -64,6 +68,7 @@ const Techonogies = () => {
                     animate="animate"
                     className="rounded-2xl border-4 border-neutral-800 p-4">
                     <SiMysql className="text-7xl text-[#00758F]" />
+                    <p className="text-center mt-2 content-evenly">Mysql</p>
                 </motion.div>
                 <motion.div
                     variants={iconVariants(2.5)}
@@ -71,6 +76,7 @@ const Techonogies = () => {
                     animate="animate"
                     className="rounded-2xl border-4 border-neutral-800 p-4">
                     <FcAcceptDatabase className="text-7xl" />
+                    <p className="text-center mt-2 content-evenly">Oracle</p>
                 </motion.div>
                 <motion.div
                     variants={iconVariants(6)}
@@ -78,6 +84,7 @@ const Techonogies = () => {
                     animate="animate"
                     className="rounded-2xl border-4 border-neutral-800 p-4">
                     <SiMongodb className="text-7xl text-green-500" />
+                    <p className="text-center mt-2 content-evenly">Mongodb</p>
                 </motion.div>
                 <motion.div
                     variants={iconVariants(4)}
@@ -85,6 +92,7 @@ const Techonogies = () => {
                     animate="animate"
                     className="rounded-2xl border-4 border-neutral-800 p-4">
                     <RiReactjsLine className="text-7xl text-cyan-400" />
+                    <p className="text-center mt-2 content-evenly">React</p>
                 </motion.div>
             </motion.div>
         </div >
--- src/constants/index.js
+++ src/constants/index.js
@@ -2,67 +2,55 @@ import a3nab from "../assets/projects/a3nab.jpg";
 import hakeem from '../assets/projects/Hakeem.png';
 import verizon from "../assets/projects/verizon.webp";
 
-export const HERO_CONTENT = `I am a Backend Developer with 4 years of experience specializing in Node.js, 
-                            Express.js, and MySQL. I have worked on complex projects in healthcare, e-commerce, 
-                            and telecommunications, creating scalable and efficient backend solutions. 
-                            I am passionate about building systems that solve real-world problems 
-                            and thrive in collaborative environments`;
+export const HERO_CONTENT = `Welcome to my portfolio! I am a Fullstack Developer with four years of experience specializing in Node.js, Express.js, React, Oracle, and MySQL. Throughout my career, I have had the opportunity to work on complex projects in the healthcare, e-commerce, and telecommunications sectors. I am passionate about creating innovative solutions and am excited to contribute my skills to new challenges.`;
 
-export const ABOUT_TEXT = `I am a highly skillfull Backend developer 
-                             I specialize in building robust and scalable APIs using Node.js and Express.js
-                             I’ve contributed to major projects for Verizon Telecommunication sector, e-commerce and Booking platforms.
-                             My expertise extends to database management with MySQL and Oracle SQL, 
-                             and I’ve played a key role in streamlining code release processes using Jenkins pipeline. 
-                             Although my primary expertise is in backend development, I’ve also built several live web 
-                             applications using React and Tailwind CSS. While I didn’t work as a frontend or 
-                             full stack developer in my past roles, I am comfortable working with React and can 
-                             effectively collaborate on frontend tasks.I’m passionate about solving complex problems 
-                             and I am actively seeking opportunities.`;
+export const ABOUT_TEXT = `I specialize in building robust and scalable APIs using Node.js and Express.js, having contributed to significant projects in the telecommunications sector with Verizon, as well as in e-commerce and booking platforms. My expertise extends to database management with MySQL and Oracle SQL, and I have developed user interfaces using React and Tailwind for various projects, including portfolios, e-commerce sites, and 3D applications. Additionally, I have played a key role in streamlining code release processes using Jenkins pipelines. I am passionate about solving complex problems and am actively seeking new opportunities to apply my skills.`;
 
 export const EXPERIENCES = [
   {
     year: "2022 - Present",
-    role: "Asociate business analyst",
+    role: "Associate Business Analyst",
     company: "Infosys",
-    description: `Involved in the development and maintenance of large-scale applications, handling real-time data, API integrations, and database management.`,
-    technologies: ["NodeJs", "Express.js", "Javascript", "Oracle", "React.js", "Git", "Jira"],
+    description: `I am involved in the development and maintenance of large-scale applications, focusing on handling real-time data, API integrations, and database management.`,
+    technologies: ["Node.js", "Express.js", "JavaScript", "Oracle", "React.js", "Git", "Jira"],
   },
   {
     year: "2020 - 2022",
     role: "Backend Developer",
-    company: "Lia infraservices",
-    description: `Worked as a Backend Developer, primarily responsible for developing and maintaining APIs and ensuring the scalability and efficiency of backend systems`,
-    technologies: ["NodeJs", "Express.js", "MySql", "Git", "AngulaJs", "Angular"],
+    company: "Lia Infraservices",
+    description: `As a Backend Developer, I was primarily responsible for developing and maintaining APIs while ensuring the scalability and efficiency of backend systems.`,
+    technologies: ["Node.js", "Express.js", "MySQL", "Git", "AngularJS", "Angular"],
   },
+
 ];
 
 export const PROJECTS = [
   {
-    title: "Booking Application Mobile and web",
+    title: "Booking Application (Mobile and Web)",
     image: hakeem,
-    description: "Developed a healthcare platform that connects patients and doctors for virtual consultations and appointment bookings. It includes mobile apps for patients and doctors, and web applications for hospital admins and platform admins",
-    responsibilties: "Developed RESTful APIs for both mobile and web applications using Node.js, Express.js, and MySQL, Integrated real-time features for doctor availability, appointment bookings, and location tracking",
-    technologies: ["NodeJs", "Express.js", "MySql", "Git", "AngulaJs"],
+    description: "Developed a healthcare platform that connects patients with doctors for virtual consultations and appointment bookings. It includes mobile applications for patients and doctors, as well as web applications for hospital administrators and owners.",
+    responsibilities: "Developed RESTful APIs for both mobile and web applications using Node.js, Express.js, and MySQL. Integrated real-time features for doctor availability, video calls, appointment bookings, patient history management, and location tracking.",
+    technologies: ["Node.js", "Express.js", "MySQL", "Git", "AngularJS"],
   },
   {
-    title: "E-Commerce Application Mobile and web",
+    title: "E-Commerce Application (Mobile and Web)",
     image: a3nab,
-    description: "Built an e-commerce platform for groceries and household items, including mobile apps for users, store owners, and delivery drivers, and an admin panel for management",
-    responsibilties: "Developed RESTful APIs for both mobile and web applications using Node.js, Express.js, and MySQL, Integrated real-time features for doctor availability, appointment bookings, and location tracking, Managed key platform functionalities such as product management, order processing, and delivery tracking.",
-    technologies: ["NodeJs", "Express.js", "MySql", "Git", "Angular"],
+    description: "Built an e-commerce platform for groceries and household items, featuring mobile applications for users, store owners, and delivery drivers, along with an admin panel for management.",
+    responsibilities: "Developed RESTful APIs for both mobile and web applications using Node.js, Express.js, and MySQL. Enabled seamless product management, store management, order processing, and delivery tracking across the platform.",
+    technologies: ["Node.js", "Express.js", "MySQL", "Git", "Angular"],
   },
   {
-    title: "Verizon telecommunication sector's Web application",
+    title: "Web Application for Verizon's Telecommunications Sector",
     image: verizon,
-    description: "Contributed to Verizon's IOP web application, providing system engineers with a centralized dashboard to manage sites, tasks, and operations",
-    responsibilties: "Developed and enhanced features using a microservices architecture with Node.js, Express.js, and Oracle SQL. Built and tested APIs, resolved production issues, and managed code releases via Jira and Jenkins. Coordinated with over 30 services, ensuring smooth integration across the system",
-    technologies: ["NodeJs", "Express.js", "Oracle", "Mongo", "Git", "React", "Jira", "Jenkins"],
+    description: "Contributed to Verizon's Integrated Operations Portal (IOP) web application, providing system engineers with a centralized dashboard to manage sites, tasks, and operations.",
+    responsibilities: "Developed and enhanced features using a microservices architecture with Node.js, Express.js, and Oracle SQL. Built and tested APIs, resolved production issues, and managed code releases via Jira and Jenkins. Coordinated with over 30 services, ensuring smooth integration across the system.",
+    technologies: ["Node.js", "Express.js", "Oracle", "MongoDB", "Git", "React", "Jira", "Jenkins"],
   },
 
 ];
 
 export const CONTACT = {
-  address: "No 2, ponniamman kovil street, sholinganallur, chennai 600019",
+  address: "No 2, ponniyamman kovil street, sholinganallur, chennai 600019",
   phoneNo: "+91 6383159335",
   email: "devendra.pmu@gmail.com",
 };
